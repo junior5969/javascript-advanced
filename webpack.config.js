d15aeef538ebaeccbfdc6b4f46c5bc2b75603b14
Chiara Barletta <clodiauni@gmail.com>
2025-07-02 17:29:34 +0200

    Fix Github Pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,39 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // Importa il plugin
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isGitHubPages = process.env.DEPLOY_ENV === 'GH_PAGES'; // Variabile di ambiente per GitHub Pages
 
 module.exports = {
-  entry: './assets/script/app.js', // Il punto di ingresso
+  entry: './assets/script/app.js', // Il punto di ingresso del tuo JS
   output: {
-  path: path.resolve(__dirname, 'dist'), // La cartella di output
-  filename: 'bundle.js', // Forza il nome del file
-  clean: true,  // Pulizia della cartella dist prima della build
-  publicPath: process.env.NODE_ENV === 'production' ? '/javascript-advanced/' : '/',
+    filename: 'bundle.js', // Il nome del file bundle che verrà generato
+    path: path.resolve(__dirname, 'dist'), // La cartella di output
+    publicPath: isGitHubPages ? '/javascript-advanced/' : '/', // Se su GitHub Pages, usa un percorso relativo
   },
-  mode: 'development', // Modalità di sviluppo
   module: {
     rules: [
       {
-        test: /\.css$/i, // Regola per i file CSS
+        test: /\.css$/i, // Riconosce i file CSS
         use: [MiniCssExtractPlugin.loader, 'css-loader'], // Usa MiniCssExtractPlugin per estrarre il CSS
       },
       {
-      test: /\.(jpg|jpeg|png|gif|svg|ico)$/i,  // Aggiungi anche ico per le icone
-      type: 'asset/resource', 
-      generator: {
-        filename: 'assets/img/[name][hash][ext][query]', // Posiziona correttamente le immagini
-        },
-      },
-      {
-        test: /\.html$/i, // Gestione dei file HTML
+        test: /\.html$/i, // Riconosce i file HTML
         loader: 'html-loader',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html', // Usa il template HTML
-      filename: 'index.html',   // Specifica il nome del file di output
+      template: './assets/index.html', // Usa il file index.html come template
+      filename: 'index.html', // Salva il file HTML nella cartella dist
     }),
     new MiniCssExtractPlugin({
-      filename: 'style.css', // Nome del file CSS
+      filename: 'style.css', // Estrae il CSS in un file separato
     }),
   ],
   devServer: {
-    contentBase: './dist',
-    open: true,
+    contentBase: path.resolve(__dirname, 'dist'), // Serve il contenuto dalla cartella dist
+    open: true, // Apre il browser automaticamente durante lo sviluppo
   },
 };
