995137dbe077e2640d9ac2b9366fcdf622e61d27
Chiara Barletta <clodiauni@gmail.com>
2025-07-02 20:48:05 +0200

    Add CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const isGitHubPages = process.env.DEPLOY_ENV === 'GH_PAGES'; // Variabile di ambiente per GitHub Pages
 
@@ -22,9 +23,17 @@ module.exports = {
         test: /\.html$/i, // Riconosce i file HTML
         loader: 'html-loader',
       },
+      {
+        test: /\.(jpg|jpeg|png|gif|svg)$/i, // Riconosce i file immagine
+        type: 'asset/resource', // Copia le immagini nella cartella dist
+        generator: {
+          filename: 'assets/img/[name][ext][query]', // Organizza le immagini nella cartella assets/img
+        },
+      },
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(), // Pulizia della cartella dist prima di ogni build
     new HtmlWebpackPlugin({
       template: './index.html', // Usa il file index.html come template
       filename: 'index.html', // Salva il file HTML nella cartella dist
